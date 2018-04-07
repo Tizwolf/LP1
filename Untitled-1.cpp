c41408e45def0581dd8b7688ff3f61e8ff18817b
Unknown <thaisysudragon@hotmail.com>
2018-04-07 14:53:46 -0500

    code:radio del circulo
    radio del circulo

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,8 +1,15 @@
-#include  (iostream)
+#include  <iostream>
 using namespace std;
-void main ()
+int main ()
 {
 
-
+    const float pi= 3.1415;
+    float radio;
+	cin >> radio;
+	float area= radio*radio*pi;
+    cout<< "el area es: "<<area<<endl;
+	  
+    return 0;
+    
     
 }
